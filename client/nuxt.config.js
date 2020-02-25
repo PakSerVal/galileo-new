f24b48985d3936cb3ffed3e0df2b4730ce856c7a
sergey <paksergey94@gmail.com>
2020-02-25 17:57:07 +1000

    yandex verification

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -8,8 +8,9 @@ module.exports = {
 			{charset: 'utf-8'},
 			{name: 'viewport', content: 'width=device-width, initial-scale=1'},
 			{hid: 'description', name: 'description', content: 'Подготовка к ЕГЭ (11 кл.) и ОГЭ (9 кл.) по всем предметам школьной программы (математика, физика, русский язык, литература, история, обществознание)'},
-			{name: 'keywords', content: 'Учебный центр Галилео город Артём, ЕГЭ и ОГЭ в Артёме, Английский язык, Корейский язык'}
-		],
+			{name: 'keywords', content: 'Учебный центр Галилео город Артём, ЕГЭ и ОГЭ в Артёме, Английский язык, Корейский язык'},
+			{name: 'yandex-verification', content: 'c086eb805c7b0dc7'},
+],
 		link:  [
 			{rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
 			{rel: 'stylesheet', type: 'text/css', href: 'https://fonts.googleapis.com/css?family=Cormorant+Garamond&display=swap'},
